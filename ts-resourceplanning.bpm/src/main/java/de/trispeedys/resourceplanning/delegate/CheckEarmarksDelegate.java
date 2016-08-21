886d4203ccd9112d41ae77c60082efbf04037b73
z000msmf <z000msmf@MD1FB0AC.ww009.siemens.net>
2016-08-21 20:01:59 +0200

    missing files

---
+++ ts-resourceplanning.bpm/src/main/java/de/trispeedys/resourceplanning/delegate/CheckEarmarksDelegate.java
@@ -0,0 +1,11 @@
+package de.trispeedys.resourceplanning.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+public class CheckEarmarksDelegate extends AbstractResourcePlanningDelegate
+{
+    public void execute(DelegateExecution execution) throws Exception
+    {
+        int werner = 5;
+    }
+}
---
+++ ts-resourceplanning.bpm/src/test/java/de/trispeedys/resourceplanning/EarmarkedPositionCancelledTest.java
@@ -0,0 +1,79 @@
+package de.trispeedys.resourceplanning;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.joda.time.LocalDate;
+import org.junit.Rule;
+import org.junit.Test;
+
+import de.gravitex.hibernateadapter.core.repository.RepositoryProvider;
+import de.trispeedys.resourceplanning.entity.Event;
+import de.trispeedys.resourceplanning.entity.Helper;
+import de.trispeedys.resourceplanning.entity.Position;
+import de.trispeedys.resourceplanning.interaction.HelperInteraction;
+import de.trispeedys.resourceplanning.interaction.enumeration.HelperCallback;
+import de.trispeedys.resourceplanning.repository.HelperRepository;
+import de.trispeedys.resourceplanning.repository.PositionEarmarkRepository;
+import de.trispeedys.resourceplanning.repository.PositionRepository;
+import de.trispeedys.resourceplanning.service.EventService;
+import de.trispeedys.resourceplanning.util.ProcessHelper;
+import de.trispeedys.resourceplanning.util.TestUtil;
+import de.trispeedys.resourceplanning.util.XmlEventParser;
+
+import static org.junit.Assert.assertEquals;
+
+public class EarmarkedPositionCancelledTest
+{
+    private static final Logger logger = Logger.getLogger(MailReminderSimpleWalkthroughProcessTest.class);
+
+    @Rule
+    public ProcessEngineRule processEngine = new ProcessEngineRule();
+
+    @Test
+    @Deployment(resources = "MailReminderProcess.bpmn")
+    public void testEarmarkedPositionCancelled()
+    {
+        TestUtil.clearAll();
+
+        // create the event
+        Event legacyEvent = new XmlEventParser().parse("testevent.xml");
+
+        // duplicate the event
+        List<LocalDate> days = new ArrayList<LocalDate>();
+        days.add(new LocalDate(2018, 6, 14));
+        days.add(new LocalDate(2018, 6, 15));
+        Event actualEvent = EventService.duplicateEvent(legacyEvent, "TRI-NEU", days, false);
+
+        // start all the processes and NOT take over legacy positions
+        List<Helper> allHelpers = RepositoryProvider.getRepository(HelperRepository.class).findAll();
+        HashMap<String, Helper> helperHash = new HashMap<String, Helper>();
+        for (Helper helper : allHelpers)
+        {
+            helperHash.put(helper.getCode(), helper);
+            ProcessHelper.startMailReminderProcess(processEngine.getRuntimeService(), actualEvent, helper);
+            HelperInteraction.processLegacyPositionTakeover(helper.getId(), actualEvent.getId(), false, processEngine.getProcessEngine());
+        }
+
+        // let helper 'H1' book 'Absperrung Herrenfeldtstrasse' (which is then completely booked)
+        Helper helperH1 = helperHash.get(TestUtil.HELPER_CODE_H1);
+        Helper helperH2 = helperHash.get(TestUtil.HELPER_CODE_H2);
+        Helper helperH3 = helperHash.get(TestUtil.HELPER_CODE_H3);
+        Position positionAbsperrungHerrenfeldtstrasse = RepositoryProvider.getRepository(PositionRepository.class).findByKey("S5", null);
+        HelperInteraction.processHelperCallback(HelperCallback.ADD_POSITION, helperH1.getId(), actualEvent.getId(), positionAbsperrungHerrenfeldtstrasse.getId(),
+                processEngine.getProcessEngine());
+        
+        // both oper helpers choose to earmark that position...
+        HelperInteraction.processHelperCallback(HelperCallback.EARMARK_POSITION, helperH2.getId(), actualEvent.getId(), positionAbsperrungHerrenfeldtstrasse.getId(),
+                processEngine.getProcessEngine());
+        HelperInteraction.processHelperCallback(HelperCallback.EARMARK_POSITION, helperH3.getId(), actualEvent.getId(), positionAbsperrungHerrenfeldtstrasse.getId(),
+                processEngine.getProcessEngine());
+        
+        // there must be two earmarks...
+        assertEquals(2, RepositoryProvider.getRepository(PositionEarmarkRepository.class).findAll().size());
+    }
+}
